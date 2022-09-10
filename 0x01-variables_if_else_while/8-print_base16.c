020ae5c5ce82249c5074862886ac2c854a9e663b
Essien <essienekemini11@gmail.com>
2022-09-09 18:13:32 -0700

    C coding

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints a hexadecimal string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0' ; d <= '9' ; d++)
+		putchar(d);
+	for (low = 'a' ; low <= 'f' ; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
