d75f87c41ac3140d2a57fb53ee7f8c45a9add647
Essien <essienekemini11@gmail.com>
2022-09-19 19:21:11 -0700

    codes

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,16 @@
 
 /**
  * _puts - prints a string followed by a new line
- * @str: an input string
- * Return: Nothing
+ * @str: pointer to a string to print
+ *
+ * Return: 0
  */
+
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		_putchar(str[i]);
 		i++;
