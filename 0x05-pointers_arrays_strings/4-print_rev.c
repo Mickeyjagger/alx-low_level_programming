6d33e211b53b79655d202876666e5a9ed3b9ded6
Essien <essienekemini11@gmail.com>
2022-09-19 19:25:14 -0700

    codes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,15 @@
 /**
  * print_rev - prints a string in reverse
  * @s: string to be printed
+ * Return: 0
  */
 void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 		i++;
-	for (i = 1 - 1; i >= 0; i--)
+	while (i--)
 		_putchat(s[i]);
 
 	_putchar('\n');
